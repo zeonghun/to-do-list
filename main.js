e05fe76007d1f93eebebccd0778f7ba9585f1455
Jeonghun Kim <positive21177@gmail.com>
2022-11-15 22:40:40 +0900

    second commit

--- main.js
+++ main.js
@@ -5,3 +5,29 @@
 // '진행중', '완료' 탭을 누르면 언더바가 이동한다.
 // '진행중' 탭을 누르면 완료되지 않은 아이템이 출력된다.
 // '완료' 탭을 누르면 완료된 아이템이 출력된다.
+
+let taskInput=document.getElementById("task-input");
+let addButton=document.getElementById("add-button");
+let taskList=[]
+addButton.addEventListener("click",addTask);
+
+function addTask(){ // 할일을 추가
+    let taskContent=taskInput.value;
+    taskList.push(taskContent);
+    console.log(taskList);
+    render();
+}
+
+function render(){  // 추가된 할일을 출력
+    let resultHTML='';
+    for(let i=0; i<taskList.length; i++){
+        resultHTML += `<div class="task">
+        <div>${taskList[i]}</div>
+        <div>
+          <button>Check</button>
+          <button>Delete</button>
+        </div>
+      </div>`;
+    }
+    document.getElementById("task-board").innerHTML=resultHTML
+}
